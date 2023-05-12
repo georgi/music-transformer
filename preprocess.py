c33e5f4c53818711cca3d00c8ab48be19780c771
Matthias Georgi <matti.georgi@gmail.com>
2023-05-12 22:52:28 +0200

    upgrade

--- preprocess.py
+++ preprocess.py
@@ -1,23 +1,11 @@
-import os
 import sys
 
-from src.performance_encoder import PerformanceEncoder
-from src.chord_melody_encoder import ChordMelodyEncoder
-from src.data import convert_midi_to_proto_folder
+from src.data import convert_maestro_to_proto
 
 if __name__ == '__main__':
-    encoder_type = sys.argv[1]
-    midi_dir = sys.argv[2]
+    dataset = sys.argv[1]
+    src_dir = sys.argv[2]
+    dest_dir = sys.argv[3]
 
-    if encoder_type == 'performnace':
-        midi_encoder = PerformanceEncoder(
-            num_velocity_bins=32,
-            steps_per_second=100
-        )
-    elif encoder_type == 'chord_melody':
-        midi_encoder = ChordMelodyEncoder()
-    else:
-        print("unknonw encoder", encoder_type)
-        exit(1)
+    convert_maestro_to_proto(src_dir, dest_dir)
 
-    convert_midi_to_proto_folder(midi_encoder, midi_dir, './data')
